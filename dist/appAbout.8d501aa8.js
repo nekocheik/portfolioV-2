9772ec790a2b88a632d749e54d68674bf464649e
Cheik <cheik.kone@hetic.net>
2019-05-18 11:16:32 +0200

    auto

--- dist/appAbout.8d501aa8.js
+++ dist/appAbout.8d501aa8.js
@@ -367,7 +367,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56848" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54390" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
